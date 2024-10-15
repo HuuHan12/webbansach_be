c70aedc93cdfde0488c61bcc79bd3f56cb7820f8
DESKTOP-TK4JPTT\dnhan <dnhan.a7.c3tqcap@gmail.com>
2024-10-15 23:28:50 +0700

    Tao Du An Moi

---
+++ src/main/java/huuhan/webbansach_be/WebbansachBeApplication.java
@@ -0,0 +1,13 @@
+package huuhan.webbansach_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbansachBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebbansachBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/huuhan/webbansach_be/WebbansachBeApplicationTests.java
@@ -0,0 +1,13 @@
+package huuhan.webbansach_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
